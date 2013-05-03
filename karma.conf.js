98b28cb68d97751d779fab435519529b17431ee4
maki <maki@bitkings.de>
2013-05-03 22:13:07 +0200

    added karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,16 @@
+files = [
+    ANGULAR_SCENARIO,
+    ANGULAR_SCENARIO_ADAPTER,
+    './dist/scripts/libs/angular.js',
+    './dist/scripts/libs/angular-resource.js',
+    './test/scripts/libs/angular-mocks.js',
+    './dist/scripts/app.js',
+    './dist/scripts/controllers/*.js',
+    './dist/scripts/directives/*.js',
+    './dist/scripts/filters/*.js',
+    './dist/scripts/responseInterceptors/*.js',
+    './dist/scripts/services/*.js',
+
+    './dist_test/scripts/filters/*.js',
+    './dist_test/scripts/services/*.js'
+];
