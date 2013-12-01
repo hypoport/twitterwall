68a25283dd483a7c2813610c795c2f7248edcab3
nitram509 <maki@bitkings.de>
2013-12-01 22:48:18 +0100

    stabilized angular module loading - fixed deps order

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
+require.config({
+  shim: {
+    'angular': {
+      deps: [],
+      exports: 'angular'
+    },
+    'angular-loader': {
+      deps: ['angular']
+    },
+    'angular-route': {
+      deps: ['angular', 'angular-loader']
+    },
+    'angular-animate': {
+      deps: ['angular', 'angular-loader']
+    }
+  }
+});
+
 require(
    ['app'],
    function (app) {
