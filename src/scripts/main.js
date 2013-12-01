74be4451660570d6d389a95f63b843445b30e841
nitram509 <maki@bitkings.de>
2013-12-01 22:40:58 +0100

    stabilized angular module loading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
-require(['app', 'libs/angular', 'libs/angular-loader', 'libs/angular-route', 'libs/angular-animate'], function(app) {
-  return angular.element(document).ready(function() {
-    return angular.bootstrap(document, ['app']);
-  });
-});
+require(
+   ['app'],
+   function (app) {
+     return angular.element(document).ready(function () {
+       return angular.bootstrap(document, ['app']);
+     });
+   });
