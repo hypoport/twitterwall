3b44e66ce00509da6d532f9c978e2a84bfe55c91
Daniel Ranke <atrusberlin@gmail.com>
2013-12-04 21:34:18 +0100

    [Single tweet view] convert visual1 to beamer optimized view. tweetController will handle with one tweet

--- src/scripts/services/twitterSearchService.js
+++ src/scripts/services/twitterSearchService.js
@@ -1,28 +1,35 @@
 define({
   type: "service",
   definition: [
-    '$log', '$timeout', '$http', '$window', function($log, $timeout, $http, $window) {
-      this.start = function(query, callback) {
+    '$log', '$timeout', '$http', '$window', function ($log, $timeout, $http, $window) {
+      var stopped = false;
+
+      this.start = function (query, callback) {
+        stopped = false;
         var max_id, repeated;
         $log.info("starting new Twitter search. Query=" + query);
         max_id = "0";
-        (repeated = function() {
+        (repeated = function () {
           return $http.get($window.appConfiguration.searchUrl, {
             'params': {
               'q': query,
               'include_entities': "true",
               'since_id': max_id
             }
-          }).success(function(data, status, headers, config) {
-            $timeout(repeated, 5000);
-            max_id = data.max_id;
-            return callback(data.tweets);
-          }).error(function(data, status, headers, config) {
-            return [];
-          });
+          }).success(function (data, status, headers, config) {
+                if (!stopped) {
+                  $timeout(repeated, 5000);
+                }
+                max_id = data.max_id;
+                return callback(data.tweets);
+              }).error(function (data, status, headers, config) {
+                return [];
+              });
         })();
       };
-      this.stop = function() {};
+      this.stop = function () {
+        stopped = true
+      };
       return this;
     }
   ]
