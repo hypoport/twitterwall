bb0b9910fecadba2c8c23c5a61871880faff5995
nitram509 <maki@bitkings.de>
2013-12-08 17:15:48 +0100

    [BoyScouting] better formatting

--- src/scripts/services/twitterSearchService.js
+++ src/scripts/services/twitterSearchService.js
@@ -1,35 +1,41 @@
 define({
   type: "service",
-  definition: [
-    '$log', '$timeout', '$http', '$window', function ($log, $timeout, $http, $window) {
-      var stopped = false;
+  definition: ['$log', '$timeout', '$http', '$window',
+    function ($log, $timeout, $http, $window) {
+
+      var _stopped = false;
 
       this.start = function (query, callback) {
-        stopped = false;
-        var max_id, repeated;
+        _stopped = false;
         $log.info("starting new Twitter search. Query=" + query);
-        max_id = "0";
+        var max_id = "0";
+        var repeated;
         (repeated = function () {
-          return $http.get($window.appConfiguration.searchUrl, {
-            'params': {
-              'q': query,
-              'include_entities': "true",
-              'since_id': max_id
-            }
-          }).success(function (data, status, headers, config) {
-                if (!stopped) {
-                  $timeout(repeated, 5000);
-                }
-                max_id = data.max_id;
-                return callback(data.tweets);
-              }).error(function (data, status, headers, config) {
-                return [];
-              });
+          $http
+             .get($window.appConfiguration.searchUrl, {
+               'params': {
+                 'q': query,
+                 'include_entities': "true",
+                 'since_id': max_id
+               }
+             })
+             .success(function (data, status, headers, config) {
+               if (!_stopped) {
+                 $timeout(repeated, 5000);
+               }
+               max_id = data.max_id;
+               callback(data.tweets);
+             })
+             .error(function (data, status, headers, config) {
+               $log.info("Error calling search service ..." + status);
+             });
         })();
       };
+
       this.stop = function () {
-        stopped = true
+        _stopped = true
       };
+
       return this;
     }
   ]
