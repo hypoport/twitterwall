e44af3b7415cc5ba9da5d077adc9dee129fc4748
nitram509 <maki@bitkings.de>
2014-01-10 21:01:43 +0100

    looping animation works  - and code simplified

--- src/scripts/controllers/singleTweetController.js
+++ src/scripts/controllers/singleTweetController.js
@@ -18,7 +18,9 @@ define({
         return array;
       }
 
-      function startAnimationDeferred_A_B() {
+      function startAnimationDeferred(srcId, dstId) {
+        dstId = dstId || 'tweetBottom';
+        srcId = srcId || 'tweetTop';
         $timeout(function () {
           function createBlurTweening(elements) {
             return function () {
@@ -32,15 +34,15 @@ define({
 
           function finalizeTweetAnimation() {
             function hideOldTwitterNameAvatar() {
-              $('#tweetTop').find('.tweet-user-avatar').animate({opacity: 0}, 1000, 'swing');
+              $('#'+ srcId).find('.tweet-user-avatar').animate({opacity: 0}, 1000, 'swing');
             }
 
             function showNewTwitterNameAvatar() {
-              $('#tweetBottom').find('.tweet-user-avatar').animate({opacity: 1}, 1000, 'swing');
+              $('#'+ dstId).find('.tweet-user-avatar').animate({opacity: 1}, 1000, 'swing');
             }
 
             function triggerTimerForNextTweet() {
-              startAnimationDeferred_B_A();
+              startAnimationDeferred(dstId, srcId);
             }
 
             (function makeAllDstElementsVisible() {
@@ -64,8 +66,8 @@ define({
             })();
           }
 
-          var src = toArray(document.getElementById("tweetTop").getElementsByClassName("letter"));
-          var dst = toArray(document.getElementById("tweetBottom").getElementsByClassName("letter"));
+          var src = toArray($('#'+srcId).find(".letter"));
+          var dst = toArray($('#'+dstId).find(".letter"));
           var dstDelta = toArray(dst);
 
           (function findMatchingLettersAndFilterThemOut() {
@@ -112,111 +114,13 @@ define({
           } else {
             movingElementsService.animate();
           }
-        }, 1);
+        }, 2000);
       }
-
-      function startAnimationDeferred_B_A() {
-        $timeout(function () {
-          var src = toArray(document.getElementById("tweetBottom").getElementsByClassName("letter"));
-          var dst = toArray(document.getElementById("tweetTop").getElementsByClassName("letter"));
-          var dstDelta = toArray(dst);
-
-          (function findMatchingLettersAndFilterThemOut() {
-            elementMap = [];
-            for (var i in src) {
-              for (var j in dstDelta) {
-                if (dstDelta[j] && src[i].textContent == dstDelta[j].textContent) {
-                  elementMap.push({src: src[i], dst: dstDelta[j]});
-                  delete dstDelta[j];
-                  delete src[i];
-                  break;
-                }
-              }
-            }
-            src = src.filter(function (x) {
-              return x != undefined;
-            });
-          })();
-
-          function createBlurTweening(elements) {
-            return function () {
-              $(elements).css({
-                "-webkit-filter": "blur(" + this.blur + "px)",
-                "-moz-filter": "blur(" + this.blur + "px)",
-                "filter": "blur(" + this.blur + "px)"
-              });
-            }
-          }
-
-          function finalizeTweetAnimation() {
-            function hideOldTwitterNameAvatar() {
-              $('#tweetBottom').find('.tweet-user-avatar').animate({opacity: 0}, 1000, 'swing');
-            }
-
-            function showNewTwitterNameAvatar() {
-              $('#tweetTop').find('.tweet-user-avatar').animate({opacity: 1}, 1000, 'swing');
-            }
-
-            function triggerTimerForNextTweet() {
-              $timeout(startAnimationDeferred_A_B(), 1);
-            }
-
-            (function makeAllDstElementsVisible() {
-              hideOldTwitterNameAvatar()
-              var runOnlyOnceGuard = false;
-              // blurring
-              $({blur: 5}).animate({blur: 0}, {
-                duration: 1000,
-                easing: 'swing', // or "linear"
-                step: createBlurTweening(dstDelta)
-              });
-              // fading
-              $(dst).animate({opacity: 1}, 1000, 'swing', function () {
-                if (!runOnlyOnceGuard) {
-                  runOnlyOnceGuard = true;
-                  movingElementsService.cleanup();
-                  showNewTwitterNameAvatar();
-                  triggerTimerForNextTweet();
-                }
-              });
-            })();
-          }
-
-          movingElementsService.addSourceElements(elementMap.map(function (e) {
-            return e.src
-          }));
-          movingElementsService.addDestinationElements(elementMap.map(function (e) {
-            return e.dst
-          }));
-          movingElementsService.prepare();
-          movingElementsService.setDoneCallback(finalizeTweetAnimation);
-
-          var runOnlyOnceGuard = false;
-          if (src.length) {
-            // blurring
-            $({blur: 0}).animate({blur: 5}, {
-              duration: 1000,
-              easing: 'swing', // or "linear"
-              step: createBlurTweening(src)
-            });
-            // fading
-            $(src).animate({opacity: 0}, 1000, "swing", function () {
-              if (!runOnlyOnceGuard) {
-                runOnlyOnceGuard = true;
-                movingElementsService.animate();
-              }
-            });
-          } else {
-            movingElementsService.animate();
-          }
-        }, 1);
-      }
-
 
       var rotateTweets = function () {
         $scope.tweetTop = tweets[0];
         $scope.tweetBottom = tweets[1];
-        startAnimationDeferred_B_A();
+        startAnimationDeferred("tweetBottom", "tweetTop");
       }
 
       twitterwallModelHolder.onSearchValueChanged(function (newSearchValue) {
