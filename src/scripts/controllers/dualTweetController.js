1149e658c4fb69a239b4bcc19f6842a51a8302f5
Daniel Ranke <atrusberlin@gmail.com>
2014-01-10 23:55:20 +0100

    both tweets are longer visible; both tweets are refreshed not at the same time

--- src/scripts/controllers/dualTweetController.js
+++ src/scripts/controllers/dualTweetController.js
@@ -3,10 +3,16 @@ define({
   definition: [
     '$scope', '$location', '$routeParams', '$log', 'tweetListHolder', '$timeout', '$filter',
     function ($scope, $location, $routeParams, $log, tweetListHolder, $timeout, $filter) {
-      $scope.tweetTime = 5000;
-      $scope.noTweets = true;
-      var _secondTweet, _rotator;
+      $scope.tweet == null;
+      $scope.tweet2 == null;
+      var _rotator;
       _listenerRegistered = false;
+
+      var _setTweetVisibility = function () {
+        $scope.noTweet1 = $scope.tweet == null;
+        $scope.noTweet2 = $scope.tweet2 == null;
+      }
+      _setTweetVisibility();
 
       if (!_listenerRegistered) {
         _listenerRegistered = true;
@@ -16,9 +22,9 @@ define({
         $log.debug("register SearchStartListener");
         tweetListHolder.registerSearchStartListener(function () {
           $log.debug("dualTweetController SearchListener started");
-          $scope.noTweets = true;
           $scope.tweet = null;
           $scope.tweet2 = null;
+          _setTweetVisibility();
           _rotator = new _Rotatator();
           _rotator.rotate();
         });
@@ -26,23 +32,31 @@ define({
 
       function _Rotatator() {
         var _enabled = true;
+        var _readTime = 10000;
 
         this.rotate = function () {
           var rotateInternal;
-          (rotateInternal = function () {
-            if (_enabled) {
-              $log.debug("dualTweetController: refresh tweets (" + $filter('date')(new Date(), "hh:mm:ss.sss") + ")");
-              _secondTweet = $scope.tweet;
-              $scope.tweet = tweetListHolder.getNextTweet();
-              if (!_secondTweet) {
-                _secondTweet = tweetListHolder.getNextTweet();
-              }
-              $scope.tweet2 = _secondTweet;
-              $scope.noTweets = $scope.tweet == null && $scope.tweet2 == null;
-              $timeout(rotateInternal, 5000);
-            }
-          })();
+          _rotateTweet1();
+          $timeout(_rotateTweet2, _readTime / 2);
         };
+
+        var _rotateTweet1 = function () {
+          if (_enabled) {
+            $log.debug("dualTweetController: refresh tweet1 (" + $filter('date')(new Date(), "hh:mm:ss.sss") + ")");
+            $scope.tweet = tweetListHolder.getNextTweet();
+            _setTweetVisibility();
+            $timeout(_rotateTweet1, _readTime);
+          }
+        }
+
+        var _rotateTweet2 = function () {
+          if (_enabled) {
+            $log.debug("dualTweetController: refresh tweet2 (" + $filter('date')(new Date(), "hh:mm:ss.sss") + ")");
+            $scope.tweet2 = tweetListHolder.getNextTweet();
+            _setTweetVisibility();
+            $timeout(_rotateTweet2, _readTime);
+          }
+        }
 
         this.stop = function () {
           _enabled = false;
