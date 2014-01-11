8abd38183ed2bf064393ac4a541df42156f059a6
oliver.otzen@hypoport.de <oliver.otzen@hypoport.de>
2014-01-11 01:33:29 +0100

    animatio for view3 established

--- src/scripts/controllers/tweetListController.js
+++ src/scripts/controllers/tweetListController.js
@@ -1,40 +1,28 @@
 define({
-  type: 'controller',
-  definition: [
-    '$scope', '$location', '$routeParams', 'twitterSearchService', 'twitterwallModelHolder', function($scope, $location, $routeParams, twitterSearchService, twitterwallModelHolder) {
-      var addNewTweet, safeApply;
-      $scope.tweets = [];
-      $scope.tweetLimit = 7;
-      safeApply = function(scope, fn) {
-        if (scope.$$phase || scope.$root.$$phase) {
-          fn();
-        } else {
-          scope.$apply(fn);
+    type: 'controller',
+    definition: [
+        '$scope', '$location', '$routeParams', '$log', 'tweetListHolder', '$timeout', function ($scope, $location, $routeParams, $log, tweetListHolder, $timeout) {
+            $scope.tweets = [];
+
+            rotateTweets = function () {
+                var tweet = tweetListHolder.getNextTweet();
+
+                if(tweet) {
+                    $log.debug("new tweet : " + tweet.text);
+                    $scope.tweets.push(tweet);
+                }
+
+                while($scope.tweets.length >= 3) {
+                    $scope.tweets.shift();
+                }
+
+//                if($scope.tweets.length < 2) {
+                    $timeout(rotateTweets, 3000);
+//                }
+
+            };
+
+            rotateTweets();
         }
-      };
-      addNewTweet = function(tweet) {
-        safeApply($scope, function() {
-          $scope.tweets.unshift(tweet);
-          $scope.tweets = $scope.tweets.slice(0, $scope.tweetLimit);
-        });
-      };
-      twitterwallModelHolder.onSearchValueChanged(function(newSearchValue) {
-        $location.path('/3/' + newSearchValue);
-      });
-      twitterwallModelHolder.onSearchValueChanged(function(newSearchValue) {
-        if (newSearchValue && newSearchValue.length > 0) {
-          twitterSearchService.start(newSearchValue, function(tweets) {
-            var tweet, _i, _len;
-            for (_i = 0, _len = tweets.length; _i < _len; _i++) {
-              tweet = tweets[_i];
-              addNewTweet(tweet);
-            }
-          });
-        }
-      });
-      if ($routeParams.query && $routeParams.query.length > 0) {
-        twitterwallModelHolder.setSearchValue($routeParams.query);
-      }
-    }
-  ]
+    ]
 });
